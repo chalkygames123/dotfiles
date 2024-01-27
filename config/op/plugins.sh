16af806b5bb5c79b4de687039aceb3651e8e01ae
Takuya Fukuju <chalkygames123@gmail.com>
2024-01-27 19:34:24 +0900

    Update config/op/plugins/used_items/gh.json and config/op/plugins/used_items/wrangler.json

--- config/op/plugins.sh
+++ config/op/plugins.sh
@@ -1,3 +1,5 @@
 export OP_PLUGIN_ALIASES_SOURCED=1
 alias aws="op plugin run -- aws"
+alias gh="op plugin run -- gh"
 alias openai="op plugin run -- openai"
+alias wrangler="op plugin run -- wrangler"
